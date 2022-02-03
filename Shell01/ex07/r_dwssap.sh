e2e99a6fb5181f1275420582ce4208f564af08be
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-03 10:24:45 +0900

    s01 end

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d ':' -f 1 | rev | sort -r | awk -v S=$FT_LINE1 -v E=$FT_LINE2 "NR>=S && NR<=E" | xargs | sed -e 's/ /, /g' | sed 's/$/./' | tr -d "\n"
+cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d ':' -f 1 | rev | sort -r | awk -v S=$FT_LINE1 -v E=$FT_LINE2 "NR>=S && NR<=E" | xargs | sed 's/ /, /g' | sed 's/$/./' | tr -d "\n"
