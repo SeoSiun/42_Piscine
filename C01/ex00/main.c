c26734bd71f773fb33ad9a4152ade064009194d4
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-05 21:23:57 +0900

    c01-ex00

---
+++ C01/ex00/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "ft_ft.c"
+
+int main(void)
+{
+	int a = 20;
+
+	ft_ft(&a);
+	printf("%d\n", a);
+}
---
+++ C01/ex01/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "ft_ultimate_ft.c"
+
+int main(void)
+{
+	int a = 20;
+
+	ft_ultimate_ft(&&&&&&&&&a);
+	printf("%d\n", a);
+}
