7282dfc843b79eb20aeb4e02f8c94eb9ffa06ee7
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-05 16:24:36 +0900

    ex08

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -5,13 +5,11 @@ void	print_result(int n, char result[])
 {
 	int i;
 
-	printf("print_result\n");
-
 	i = 0;
 	write(1, result, n);
 	while (i < n)
 	{
-		if (result[i] - '0'  == (10 - (n - i)))
+		if (result[i] - '0'  != (10 - (n - i)))
 		{
 			write(1, ", ", 2);
 			break;
@@ -22,11 +20,13 @@ void	print_result(int n, char result[])
 
 void	print_comb_recursive(int n, char result[], int index, int value)
 {
-	if (n <= index)
+	if (index == n)
+	{
 		print_result(n, result);
+		return;
+	}
 	while (value <= (10 - (n -index)))
 	{
-		printf("value: %d\n", value);
 		result[index] = value + '0';
 		print_comb_recursive(n, result, index + 1, value + 1);
 		value++;
