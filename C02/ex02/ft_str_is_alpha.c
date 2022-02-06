613390db2fa4bbd9ea183416fedcda5468739be0
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-06 17:44:56 +0900

    c02

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,18 @@
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+	int is_alpha;
+
+	is_alpha = 1;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] <= 'a' || str[i] >= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			is_alpha = 0;
+			break;
+		}
+	}
+
+
+}
