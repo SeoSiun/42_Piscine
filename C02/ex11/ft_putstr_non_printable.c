b4ea7c93ac77c154cf0188c9fcfb41408c2508a4
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-08 19:27:25 +0900

    the end

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,20 +6,20 @@
 /*   By: siseo <siseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 19:33:37 by siseo             #+#    #+#             */
-/*   Updated: 2022/02/08 18:06:39 by siseo            ###   ########.fr       */
+/*   Updated: 2022/02/08 19:08:40 by siseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	print_char_as_hex(char c)
+void	print_char_as_hex(unsigned char c)
 {
 	char	*hex_list;
 
 	hex_list = "0123456789abcdef";
 	write(1, "\\", 1);
-	write(1, &hex_list[c>>4 & 15], 1);
-	write(1, &hex_list[c & 15], 1);
+	write(1, &hex_list[c / 16], 1);
+	write(1, &hex_list[c % 16], 1);
 }
 
 void	ft_putstr_non_printable(char *str)
@@ -31,7 +31,7 @@ void	ft_putstr_non_printable(char *str)
 	{
 		if (str[i] < 32 || str[i] > 126)
 		{
-			print_char_as_hex(str[i]);
+			print_char_as_hex((unsigned char)str[i]);
 		}
 		else
 			write(1, &str[i], 1);
