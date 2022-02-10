cceea0110ae9d4b9a6e64a27b99bdd2fde2361da
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-10 21:27:48 +0900

    c05 ~ex08

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-int	ft_is_prime(int nb)
+int	_ft_is_prime(int nb)
 {
 	int	num;
 
@@ -14,4 +12,14 @@ int	ft_is_prime(int nb)
 		num++;
 	}
 	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = nb;
+	while (!_ft_is_prime(i))
+		i++;
+	return (i);
 }
---
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+
+void	print_queens_pos(int *queens)
+{
+	int		i;
+	char	tmp;
+
+	i = 0;
+	while (i < 10)
+	{
+		tmp = queens[i] + '0';
+		write(1, &tmp, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	check_queens(int *queens, int col)
+{
+	int	i;
+
+	i = 0;
+	while (i < col)
+	{
+		if ((col - i == queens[col] - queens[i])
+				 || (col - i == queens[i] - queens[col]))
+			return (0);
+		if (queens[i] == queens[col])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_ten_queens_recursive(int *queens, int col, int* sol_cnt)
+{
+	int	row;
+	
+	if (col == 10)
+	{
+		print_queens_pos(queens);
+		*sol_cnt = *sol_cnt + 1;
+		return ;
+	}
+	row = 0;
+	while (row < 10)
+	{
+		queens[col] = row;
+		if (check_queens(queens, col))
+			ft_ten_queens_recursive(queens, col + 1, sol_cnt);
+		row++;
+	}
+}
+
+int	ft_ten_queens_puzzle(void)
+{
+	int	queens[10];
+	int	sol_cnt;
+
+	sol_cnt = 0;
+	ft_ten_queens_recursive(queens, 0, &sol_cnt);
+	return (sol_cnt);
+}
