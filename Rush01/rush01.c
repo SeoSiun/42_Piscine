9cf2f9385d6d6527d330f9888fc6a7e96929d32c
SeoSiun <siun0816@hanyang.ac.kr>
2022-02-12 13:36:10 +0900

    rush01

--- Rush01/rush01.c
+++ Rush01/rush01.c
@@ -1,24 +1,122 @@
-int	up_valid(int **board, int *up)
-{
+#include <unistd.h>
 
+int	up_valid(int board[4][4], int *up)
+{
+	int	i;
+	int	j;
+	int	cnt;
+	int	max;
+
+	i = 0;
+	while (i < 4)
+	{
+		cnt = 0;
+		max = 0;
+		j = 0;
+		while (j < 4)
+		{
+			if (max < board[j][i])
+			{
+				max = board[j][i];
+				cnt++;
+			}
+			j++;
+		}
+		if (cnt != up[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int	down_valid(int **board, int *down)
+int	down_valid(int board[4][4], int *down)
 {
+	int	i;
+	int	j;
+	int	cnt;
+	int	max;
 
+	i = 0;
+	while (i < 4)
+	{
+		cnt = 0;
+		max = 0;
+		j = 0;
+		while (j < 4)
+		{
+			if (max < board[3-j][i])
+			{
+				max = board[3-j][i];
+				cnt++;
+			}
+			j++;
+		}
+		if (cnt != down[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int	left_valid(int **board, int *left)
+int	left_valid(int board[4][4], int *left)
 {
+	int	i;
+	int	j;
+	int	cnt;
+	int	max;
 
+	i = 0;
+	while (i < 4)
+	{
+		cnt = 0;
+		max = 0;
+		j = 0;
+		while (j < 4)
+		{
+			if (max < board[i][j])
+			{
+				max = board[i][j];
+				cnt++;
+			}
+			j++;
+		}
+		if (cnt != left[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int right_valid(int **board, int *right)
+int right_valid(int board[4][4], int *right)
 {
+	int	i;
+	int	j;
+	int	cnt;
+	int	max;
 
+	i = 0;
+	while (i < 4)
+	{
+		cnt = 0;
+		max = 0;
+		j = 0;
+		while (j < 4)
+		{
+			if (max < board[i][3 - j])
+			{
+				max = board[i][3 - j];
+				cnt++;
+			}
+			j++;
+		}
+		if (cnt != right[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int	valid_check(int** board, int *view)
+int	valid_check(int board[4][4], int view[16])
 {
 	if (!up_valid(board, view))
 		return (0);
@@ -26,12 +124,12 @@ int	valid_check(int** board, int *view)
 		return (0);
 	if (!left_valid(board, &view[8]))
 		return (0);
-	if (!right_valid(board, &view[12]));
+	if (!right_valid(board, &view[12]))
 		return (0);
 	return (1);
 }
 
-int	is_exist_same_line(board, row, col)
+int	is_exist_same_line(int board[4][4], int row, int col)
 {
 	int i;
 	
@@ -52,12 +150,30 @@ int	is_exist_same_line(board, row, col)
 	return (0);
 }	
 
-void	print_board(int **board)
+void	print_board(int board[4][4])
 {
+	int	i;
+	int	j;
+	char tmp;
 
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			tmp = board[i][j] + '0';
+			write(1, &tmp, 1);
+			if (j != 3)
+				write(1, " ", 1);
+			j++; 
+		}
+		write(1, "\n", 1);
+		i++;
+	}
 }
 
-void	set_board(int **board, int row, int col, int *view)
+void	set_board(int board[4][4], int row, int col, int view[16])
 {
 	int	i;
 	if (row == 4 && valid_check(board, view))
@@ -77,7 +193,7 @@ void	set_board(int **board, int row, int col, int *view)
 	}
 }
 
-int	is_valid_arg(char *str, int *view)
+int	is_valid_arg(char *str, int view[16])
 {
 	int	i;
 	int	cnt;
@@ -97,21 +213,19 @@ int	is_valid_arg(char *str, int *view)
 			return (0);
 		i++;
 	}
-	if (cnt != 4)
+	if (cnt != 16 || i != 31)
 		return (0);
 	return (1);
 }
 
-void	init_board(int** board)
+void	init_board(int board[4][4])
 {
 	int i;
 	int j;
 
-	board = malloc(sizeof(int *) * 4);
 	i = 0;
 	while (i < 4)
 	{
-		board[i] = malloc(sizeof(int) * 4);
 		j = 0;
 		while (j < 4)
 		{
@@ -124,10 +238,9 @@ void	init_board(int** board)
 
 int	main(int argc, char *argv[])
 {
-	int	*view;
-	int **board;
+	int	view[16];
+	int board[4][4];
 
-	view = malloc(sizeof(int) * 4);
 	if (argc != 2 || !is_valid_arg(argv[1], view))
 	{
 		write(1, "Error\n", 6);
